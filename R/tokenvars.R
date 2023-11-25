1bf14dde189909a007345a135b1b063f3ea43471
chainsawriot <chainsawtiney@gmail.com>
2023-11-25 10:56:08 +0100

    helper function to add token_id

--- R/tokenvars.R
+++ R/tokenvars.R
@@ -1,3 +1,13 @@
 tokenvars <- function(x, field) {
     ## place holder
 }
+
+add_token_id <- function(x) {
+    ## using attr(vec, "names") as token_id
+    unclassed_x <- unclass(x)
+    for (i in seq_along(unclassed_x)) {
+        attr(unclassed_x[[i]], "names") <- paste0("t", seq_along(unclassed_x[[i]]))
+    }
+    class(unclassed_x) <- c("tokens")
+    return(unclassed_x)
+}
